a8e7d55fbf2a17e8e9969f9f058fd011ebe7001c
Robert Nicholson <bob@busapp.com>
2021-12-23 21:32:52 -0600

    Changes for AWS

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Multi-Docker Here and Now. Or maybe Just React.
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>

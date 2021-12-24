09a29a588e5ea43a2c8a80b78d076f89ef0f1cc6
Robert Nicholson <bob@busapp.com>
2021-12-23 21:39:15 -0600

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -7,27 +6,17 @@ import Fib from './Fib';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Multi-Docker Here and Now. Or maybe Just React.
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
+      </header>
+      <div>
+        <Route exact path="/" component={Fib} />
+        <Route path="/otherpage" component={OtherPage} />
       </div>
-    </Router>
+    </div>
   );
 }
 
